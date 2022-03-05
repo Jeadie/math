a59179ead5af054aa96b0fe7f01a8b9f188c11c9
Jack Eadie <jeadi@amazon.com>
2022-03-05 15:06:22 +1000

    restructure

--- chaos/main.go
+++ chaos/main.go
@@ -3,30 +3,41 @@ package chaos
 import (
 	"flag"
 	"fmt"
-	"strconv"
 )
 
 type ChaosParams struct {
 	x float64
 	k float64
+
+	initalIter uint // Number of initial iterations before considering output
+	dy float64 // delta between iterations before halting (after initialIter), if not terminated for cyclic nature
+}
+
+func GetFlagAndParams(p *ChaosParams) *flag.FlagSet {
+	fs := flag.NewFlagSet("chaos", flag.ExitOnError)
+	fs.Float64Var(&p.x, "x", 0.42, "Parameter of the recursive formula kx**2 -1")
+	fs.Float64Var(&p.k, "k", 0.24, "Parameter of the recursive formula kx**2 -1")
+	fs.UintVar(&p.initalIter, "initial-iterations", 20, "Number of initial iterations before considering output")
+	fs.Float64Var(&p.dy, "delta", 0.001, "delta between iterations before halting (after initialIter), if not terminated for cyclic nature")
+	return fs
 }
 
 func GetParams(args []string) *ChaosParams {
-	fs := flag.NewFlagSet("chaos", flag.ExitOnError)
-	fs.Parse(args)
 	param := ChaosParams{}
-	x, err := strconv.ParseFloat(fs.Arg(0), 64)
-	if err != nil {
-		fmt.Println(err)
-		return &param
-	}
-	param.x = x
-	param.k, err = strconv.ParseFloat(fs.Arg(1), 64)
-	if err != nil {fmt.Println(err)}
+	fs := GetFlagAndParams(&param)
+	fs.Parse(args)
 	return &param
 }
 
+func Help() {
+	GetFlagAndParams(&ChaosParams{}).Usage()
+}
+
 func Chaos(args []string) {
+	if len(args) == 0 {
+		Help()
+		return
+	}
 	params := GetParams(args)
 	fmt.Println("CHAOSSS", params)
 }
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/Jeadie/math/chaos"
 	"os"
 )
@@ -9,10 +10,24 @@ const (
 	Chaos string = "chaos"
 )
 
+func ListCommands() {
+	fmt.Println("Available commands: ")
+	fmt.Println(fmt.Sprintf("    %s: run iteration experiment of kx**2 -1", Chaos))
+}
+
 func main() {
+	if len(os.Args) < 2 || os.Args[1] == "--help" || os.Args[1] == "-h" {
+		ListCommands()
+		return
+	}
+
 	switch os.Args[1] {
 		case Chaos:
 			chaos.Chaos(os.Args[2:])
+
+		default:
+			ListCommands()
+			return
 	}
 
 }
