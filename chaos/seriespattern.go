fc7e5d7a0e028e16ad42ef1df303af0ab3519d9b
Jack Eadie <jeadi@amazon.com>
2022-03-06 14:01:39 +1000

    implement convergent, divergent and periodic series checks

--- chaos/seriespattern.go
+++ chaos/seriespattern.go
@@ -13,7 +13,7 @@ const (
 )
 // SeriesChecks is list of functions where the i'th State check returns true iff pattern is SeriesPattern(i)
 // Chaotic must be last check. Chaotic if no other pattern found.
-var seriesChecks = []func([]float64) bool{isFalse, isConvergent, isFalse, isFalse, isChaotic, isTrue}
+var seriesChecks = []func([]float64) bool{isDivergent, isConvergent, isPeriodic, isFalse, isTrue}
 var patternNames = []string{"Divergent", "Convergent", "Periodic", "Intermittency", "Chaotic"}
 
 func FindPattern(series []float64) SeriesPattern {
@@ -33,10 +33,61 @@ func (p SeriesPattern) ToString() string {
 func isFalse(x []float64) bool { return false }
 func isTrue(x []float64) bool { return true }
 
-func isChaotic(x []float64) bool {
+func isPeriodic(x []float64) bool {
+
+	// map value to occurrences
+	f := make(map[float64][]int)
+	for i, v := range x {
+		l, exist := f[v]
+		if exist {
+			l = append(l, i)
+		} else {
+			f[v] = []int{i}
+		}
+	}
+
+	// First element must recur
+	occ := f[x[0]]
+	if len(occ) < 2 {
+		return false
+	}
+
+	// Try all periodicities, smallest first, from first element
+	for _, p := range occ[1:] {
+		if hasPeriod(x, p) {
+			return true
+		}
+	}
+	return false
+}
+
+func hasPeriod(x []float64, p int) bool {
+	for i := range x[:len(x)-p] {
+		if x[i] != x[i+p] {
+			return false
+		}
+	}
 	return true
 }
 
+func isDivergent(x []float64) bool {
+	nInf := math.Inf(-1)
+	pInf := math.Inf(+1)
+
+	for _, v := range x {
+		if v == nInf || v == pInf {
+			return true
+		}
+	}
+	return false
+}
+
 func isConvergent(x []float64) bool {
-	return math.Abs(x[0] -  x[1]) < 0.0001
+	y := x[0]
+	for _, v := range x {
+		if math.Abs(y-v) > 0.0001 {
+			return false
+		}
+	}
+	return true
 }
